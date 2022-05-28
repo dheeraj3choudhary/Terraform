09d287b46a9af2ec09291c8a3af8c5203d8a354c
Dheeraj Choudhary <88716270+dheeraj3choudhary@users.noreply.github.com>
2022-05-28 21:36:41 +0530

    Update provider.tf

--- VPC_IGW/provider.tf
+++ VPC_IGW/provider.tf
@@ -1,4 +1,4 @@
 provider "aws" {
   region                  = "us-east-1"
-  shared_credentials_file = "C:/Users/DHEERA~1/.aws/credentials"
+  shared_credentials_file = "<Your AWS Credentials File path>"
 }
